754174bf432cd4d0bfb4c97d40c46a5fb2c3168c
arkteck <arkteck@gmail.com>
2022-05-02 19:14:25 -0400

    Complete updating a todo

--- datastore/index.js
+++ datastore/index.js
@@ -51,13 +51,22 @@ exports.readOne = (id, callback) => {
 };
 
 exports.update = (id, text, callback) => {
-  var item = items[id];
-  if (!item) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    items[id] = text;
-    callback(null, { id, text });
-  }
+
+  fs.exists(path.join(exports.dataDir, id + '.txt'), (exists) => {
+    if (!exists) {
+      callback(new Error(`No item with id: ${id}`));
+    } else {
+      fs.writeFile(path.join(exports.dataDir, id + '.txt'), text, (err) => {
+        if (err) {
+          callback(err);
+        } else {
+          callback(null, {id, text});
+        }
+      });
+
+    }
+  });
+
 };
 
 exports.delete = (id, callback) => {
